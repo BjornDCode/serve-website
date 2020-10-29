5cf2894230f58a1909f1cbf51c6499413319764a
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-10-29 11:17:24 +0100

    Try styles

--- .eleventy.js
+++ .eleventy.js
@@ -3,14 +3,11 @@ const htmlmin = require('html-minifier')
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false)
 
-  eleventyConfig.addWatchTarget('./_tmp/style.css')
-
-  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
   eleventyConfig.addPassthroughCopy({ static: 'static' })
 
-  eleventyConfig.addPassthroughCopy({
-    './node_modules/alpinejs/dist/alpine.js': './js/alpine.js',
-  })
+  // eleventyConfig.addPassthroughCopy({
+  //   './node_modules/alpinejs/dist/alpine.js': './js/alpine.js',
+  // })
 
   eleventyConfig.addShortcode('version', function () {
     return String(Date.now())
--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  plugins: [
-    require(`tailwindcss`)(`./styles/tailwind.config.js`),
-    require(`autoprefixer`),
-    ...(process.env.NODE_ENV === "production"
-      ? [require(`postcss-clean`)]
-      : []),
-  ],
-};
+    plugins: [
+        require(`tailwindcss`)(`./tailwind.config.js`),
+        require(`autoprefixer`),
+        ...(process.env.NODE_ENV === 'production'
+            ? [require(`postcss-clean`)]
+            : []),
+    ],
+}
